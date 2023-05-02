5fc825c3fd6a5124f3cd63c1085a0f3b91e1f48a
Ítalo Nobre <italohenriquenobreeoq@gmail.com>
2023-05-02 11:50:18 -0300

    Criação da tela de propriedades

---
+++ app/properties/page.tsx
@@ -0,0 +1,50 @@
+import EmptyState from "../components/EmptyState";
+import ClientOnly from "../components/ClientOnly";
+import PropertiesClient from "./PropertiesClient"
+
+import getCurrentUser from "../actions/getCurrentUser";
+import TripsClient from "./PropertiesClient";
+import getListings from "../actions/getListings";
+
+
+const PropertiesPage = async () => {
+  const currentUser = await getCurrentUser()
+  
+
+  if (!currentUser) {
+    return (
+      <ClientOnly>
+        <EmptyState 
+          title="Não autorizado"
+          subtitle="Por favor faça o login"
+        />
+      </ClientOnly>
+    )
+  }
+
+  const listings = await getListings({
+    userId: currentUser.id
+  });
+
+  if (listings.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState 
+          title="Nenhuma propriedade encontrada"
+          subtitle="Parece que você não tem propriedades"
+        />
+      </ClientOnly>
+    )
+  }
+
+  return (
+    <ClientOnly>
+      <PropertiesClient 
+        listings={listings}
+        currentUser={currentUser}
+      />
+    </ClientOnly>
+  )
+}
+
+export default PropertiesPage;
